ef3a8981da786faf2051c38ed476609350ed963f
Riccardo Masutti <46527252+RiccardoMasutti@users.noreply.github.com>
2019-12-13 12:02:48 +0100

    Replace .onion icon with clearnet
    Closes #2756

--- WalletWasabi.Backend/WebsiteTorifier.cs
+++ WalletWasabi.Backend/WebsiteTorifier.cs
@@ -28,7 +28,8 @@ namespace WalletWasabi.Backend
 			content = content.Replace("http://wasabiukrxmkdgve5kynjztuovbg43uxcbcxn6y2okcrsg7gb6jdmbad.onion", "https://wasabiwallet.io", StringComparison.Ordinal);
 			content = content.Replace("https://blockstream.info", "http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion", StringComparison.Ordinal);
 			content = content.Replace("https://wasabiwallet.io/swagger/", "http://wasabiukrxmkdgve5kynjztuovbg43uxcbcxn6y2okcrsg7gb6jdmbad.onion/swagger/", StringComparison.Ordinal);
-			
+			content = content.Replace("images/icon-clearnet.png?ver=1.1.0", "http://wasabiukrxmkdgve5kynjztuovbg43uxcbcxn6y2okcrsg7gb6jdmbad.onion/swagger/", StringComparison.Ordinal);
+
 			await File.WriteAllTextAsync(onionPath, content);
 		}
 	}
